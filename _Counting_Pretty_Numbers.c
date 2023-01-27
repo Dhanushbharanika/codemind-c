99061e36097243005b1c90e50bd9a3b8d5ea22dd
Dhanushbharanika <119720388+Dhanushbharanika@users.noreply.github.com>
2023-01-27 18:29:38 +0530

    #include<stdio.h> int main() {     int tc,k;     scanf("%d",&tc);     for(k=1;k<=tc;k++)     {         int a,b,i,c=0;         scanf("%d %d",&a,&b);         for(i=a;i<=b;i++)         {             if(i%10==2 || i%10==3 || i%10==9)             c++;             }             printf("%d",c);             printf(" ");             }
    }

--- _Counting_Pretty_Numbers.c
+++ _Counting_Pretty_Numbers.c
@@ -6,14 +6,15 @@ int main()
     for(k=1;k<=tc;k++)
     {
         int a,b,i,c=0;
-        scanf("%d %d",&a,&b);
+        scanf("%d %d",&a,&b); 
         for(i=a;i<=b;i++)
         {
             if(i%10==2 || i%10==3 || i%10==9)
             c++;
-        }
-        printf("%d",c);
-        printf("
+            }
+            printf("%d",c);
+            printf("
 ");
-    }
+            }
+    
 }
